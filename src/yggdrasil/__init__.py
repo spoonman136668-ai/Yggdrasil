6121c5d3e24f2aff241b1af195a9341b2f8e7db8
spoonman136668-ai <spoonman136668@gmail.com>
2026-09-17 13:10:45 -0400

    dg1a: initialize research package

---
+++ src/yggdrasil/__init__.py
@@ -0,0 +1,5 @@
+"""Yggdrasil developmental-intelligence research package."""
+
+from .nca import NCAConfig, NeuralCellularAutomaton, make_seed_state
+
+__all__ = ["NCAConfig", "NeuralCellularAutomaton", "make_seed_state"]
